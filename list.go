998066f3a63b5a1c8df869b1b375e7465e17e1c2
zhudejiang <dakgzhu@foxmail.com>
2019-01-07 19:32:17 +0800

    add list command

--- list.go
+++ list.go
@@ -1,1 +1,118 @@
 package main
+
+import (
+	"container/list"
+)
+
+type RedisList struct {
+	Objects *list.List
+}
+
+func NewRedisList() *RedisList{
+	return &RedisList{
+		Objects:list.New(),
+	}
+}
+
+func (l *RedisList) Push(argv []string) int {
+	for _,v := range argv{
+		l.Objects.PushFront(v)
+	}
+	return l.Objects.Len()
+}
+
+func (l *RedisList) Insert(op string,pivot string,value string) int {
+	if l.Objects.Len() == 0 {
+		return 0
+	}
+	for e := l.Objects.Front(); e != nil; e = e.Next() {
+		if e.Value.(string) == pivot {
+			if op == "BEFORE" {
+				l.Objects.InsertBefore(value,e)
+			} else {
+				l.Objects.InsertAfter(value,e)
+			}
+
+			return l.Objects.Len()
+		}
+	}
+
+	return -1
+}
+
+func (l *RedisList) Len() int {
+	return l.Objects.Len()
+}
+
+func (l *RedisList) LPop() string {
+	e := l.Objects.Front()
+	l.Objects.Remove(e)
+	return e.Value.(string)
+}
+func (l *RedisList) RPop() string {
+	e := l.Objects.Back()
+	l.Objects.Remove(e)
+	return e.Value.(string)
+}
+
+func (l *RedisList) Rem(count int,value string) int {
+	ret := 0
+	if count > 0 {
+		for e := l.Objects.Front(); e != nil; e = e.Next() {
+			if e.Value.(string) == value {
+				l.Objects.Remove(e)
+				ret++
+				if ret == count {
+					return count
+				}
+			}
+		}
+	} else if count < 0 {
+		for e := l.Objects.Back(); e != nil; e = e.Prev() {
+			if e.Value.(string) == value {
+				l.Objects.Remove(e)
+				ret--
+				if ret == count {
+					return -count
+				}
+			}
+		}
+	} else {
+		for e := l.Objects.Front(); e != nil; e = e.Next() {
+			if e.Value.(string) == value {
+				l.Objects.Remove(e)
+				ret++
+			}
+		}
+	}
+	return ret
+}
+
+func (l *RedisList) Set(index int,value string) error {
+	ret := 0
+	if index >= 0 {
+		if index >= l.Objects.Len() {
+			return ErrIndexOutOfRange
+		}
+		for e := l.Objects.Front(); e != nil; e = e.Next() {
+			if index == ret {
+				e.Value = value
+				return nil
+			}
+			ret++
+		}
+	} else {
+		if -index > l.Objects.Len() {
+			return ErrIndexOutOfRange
+		}
+		for e := l.Objects.Back(); e != nil; e = e.Prev() {
+			ret--
+			if index == ret {
+				e.Value = value
+				return nil
+			}
+		}
+	}
+
+	return ErrIndexOutOfRange
+}
--- xerrors.go
+++ xerrors.go
@@ -8,12 +8,10 @@ var (
 	ErrRedisLength = errors.New("ErrRedisLength")
 	ErrKeyNotFound = errors.New("key not found")
 	ErrTypeNotMatch = errors.New("type not match")
-)
-
-var (
 	ErrDontSupportThisCommand = errors.New("don't support this command  !!!!!!")
 	ErrCommandArgsWrongNumber = errors.New("ERR wrong number of arguments for '%s' command")
 	ErrCommand                = errors.New("ERR command")
 	ErrValueType         = errors.New("ERR value is not an integer or out of range")
 	ErrWrongKeyType         = errors.New("WRONGTYPE Operation against a key holding the wrong kind of value")
+	ErrIndexOutOfRange        = errors.New("ERR index out of range")
 )
